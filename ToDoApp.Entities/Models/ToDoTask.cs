6a1dd103bf68bc61c6b45b108117a28c2f1e8d55
vladimir-sinkovets <vovasin49@gmail.com>
2023-03-02 19:22:54 +0300

    Added entity models and create todoTask command for mediatr

---
+++ ToDoApp.Entities/Models/ToDoTask.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoApp.Entities.Models
+{
+    public class ToDoTask
+    {
+        public Guid Id { get; set; }
+        public DateTime Deadline { get; set; }
+        public ToDoTaskData Data { get; set; }
+
+    }
+}
---
+++ ToDoApp.UseCases/Features/ToDoTask/Commands/CreateToDoTask/CreateToDoTaskCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace ToDoApp.UseCases.Features.ToDoTask.Commands.CreateToDoTask
+{
+    public class CreateToDoTaskCommandHandler : IRequestHandler<CreateToDoTaskCommand, Guid>
+    {
+        public Task<Guid> Handle(CreateToDoTaskCommand request, CancellationToken cancellationToken)
+        {
+            
+            return default;
+        }
+    }
+}
