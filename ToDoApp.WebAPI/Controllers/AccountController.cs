6044563a7571ad3b191524d37002f1bb9a5639dc
vladimir-sinkovets <vovasin49@gmail.com>
2023-03-10 16:18:19 +0300

    Implemented AccountController register action

---
+++ ToDoApp.WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApp.UseCases.Account.Commands.RegisterUser;
+
+namespace ToDoApp.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public AccountController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("logout")]
+        public IActionResult Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register(RegisterUserCommand registerUserCommand)
+        {
+            var result = await _mediator.Send(registerUserCommand);
+
+            return result.IsSuccessed == true ? 
+                Json(new { IsSuccessed = result.IsSuccessed }) : 
+                Json(result);
+        }
+    }
+}
